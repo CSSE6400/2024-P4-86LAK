0c3425df7a1b5298260c4125f127986fa6a392d8
86LAK <lakshan392@gmail.com>
2024-03-13 10:54:50 +1000

    added hextris

--- main.tf
+++ main.tf
@@ -20,10 +20,42 @@ provider "aws" {
 }
 
 resource "aws_instance" "hextris-server" {
-ami = "ami-0e731c8a588258d0d"
-instance_type = "t2.micro"
-key_name = "vockey"
-tags = {
-Name = "hextris"
+  ami = "ami-0e731c8a588258d0d"
+  instance_type = "t2.micro"
+  key_name = "vockey"
+  tags = {
+    Name = "hextris"
+  }
+  user_data = file("./serve-hextris.sh")
 }
+
+
+resource "aws_security_group" "hextris-server" {
+  name = "hextris-server"
+  description = "Hextris HTTP and SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "hextris-url" {
+  value = aws_instance.hextris-server.public_ip
 }
