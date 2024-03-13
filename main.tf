88d962febde47745c8e85257c77d50725a726918
86LAK <lakshan392@gmail.com>
2024-03-13 10:31:25 +1000

    blah

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ provider "aws" {
         }
   }
 }
+
+resource "aws_instance" "hextris-server" {
+ami = "ami-0e731c8a588258d0d"
+instance_type = "t2.micro"
+key_name = "vockey"
+tags = {
+Name = "hextris"
+}
+}
